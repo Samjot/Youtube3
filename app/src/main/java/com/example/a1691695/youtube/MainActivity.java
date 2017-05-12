c227de2b7c7873d2a28e006d4b2f00a28e890423
Samjot <ssmjot@gmail.com>
2017-05-12 10:30:18 -0400

    By Sam

--- app/src/main/java/com/example/a1691695/youtube/MainActivity.java
+++ app/src/main/java/com/example/a1691695/youtube/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.a1691695.youtube;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+    }
+
+    public void SubClick (View v){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+    public void SharedClick (View v){
+        Intent intent = new Intent(this,Shared.class);
+        startActivity(intent);
+    }
+
+    public void HomeClick (View v){
+        Intent intent = new Intent(this,Subscription.class);
+        startActivity(intent);
     }
 
 
