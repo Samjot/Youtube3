26eed8cb4750bdfaf2245c8f646c2ca164d7d627
Samjot <ssmjot@gmail.com>
2017-05-15 14:39:14 -0400

    9 Screens final

--- app/src/main/java/com/example/a1691695/youtube/Trending.java
+++ app/src/main/java/com/example/a1691695/youtube/Trending.java
@@ -8,6 +8,6 @@ public class Trending extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.Playvideo);
+        setContentView(R.layout.activity_playvideo);
     }
 }
